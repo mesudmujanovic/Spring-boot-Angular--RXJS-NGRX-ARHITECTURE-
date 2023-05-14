07bcae25e6c3912b99dde8afaac4e888748a299a
Mesud Mujanovic <93776713+mesudmujanovic@users.noreply.github.com>
2023-05-14 21:24:19 +0200

    Delete app directory

--- app/store/reducers/app.reducers.ts
+++
@@ -1,8 +0,0 @@
-import { AppState } from "../state/app.state";
-import { ActionReducerMap } from "@ngrx/store";
-import { userOdrerReducers } from "./reducers";
-
-
-export const appReducers: ActionReducerMap<AppState, any>= {
-    order: userOdrerReducers
-}
